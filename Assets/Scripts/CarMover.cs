894342a3c0fb6e419cf3e066ccd34c303a0fdd0d
Ruslan <ruslan.kulbitski@gmail.com>
2018-07-27 11:43:18 +0300

    release v1

---
+++ Assets/Scripts/CarMover.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class CarMover : MonoBehaviour {
+
+    public Transform SelfTransform;
+    public Tilemap Map;
+    public TileBase GroundTile;
+    public float Speed;
+
+    private Vector3 _force;
+    private bool _isAccelerated;
+
+    public float GetForce()
+    {
+        return _force.magnitude;
+    }
+
+	public void Accelerate()
+    {
+        _force += (SelfTransform.up * Time.deltaTime) * 0.1f;
+        _isAccelerated = true;
+    }
+
+    public void RotateRight()
+    {
+        SelfTransform.Rotate(0, 0, -1);
+    }
+
+    public void RotateLeft()
+    {
+        SelfTransform.Rotate(0, 0, 1);
+    }
+
+	void LateUpdate ()
+    {
+        if (!_isAccelerated)
+        {
+            _force = Vector3.Lerp(_force, Vector3.zero, Time.deltaTime);
+        }
+
+        if(GroundTile == Map.GetTile(Map.WorldToCell(SelfTransform.position)))
+        {
+            _force *= 0.9f;
+        }
+
+        SelfTransform.position += _force * Speed * Time.deltaTime;
+        
+
+        _isAccelerated = false;
+    }
+}
---
+++ Assets/Scripts/RandomSpeed.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSpeed : MonoBehaviour {
+
+    public CarMover Car;
+
+    void Start ()
+    {
+        Car.Speed = Random.Range(1f, 10f);
+	}
+}
